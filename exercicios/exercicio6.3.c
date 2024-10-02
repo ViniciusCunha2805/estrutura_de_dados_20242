35f879451a9362ff31f618acf1ed3b0479d85a4c
Vinícius da Silva Cunha <113987586+ViniciusCunha2805@users.noreply.github.com>
2024-10-02 10:39:34 -0300

    Create exercicio6.3.c

---
+++ exercicios/exercicio6.3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct t_ponto {
+    int x; 
+    int y;
+};
+
+double distancia(struct t_ponto A, struct t_ponto B){
+    double dist = sqrt(pow((A.x - B.x), 2) + pow((A.y - B.y), 2));
+    return dist;
+}
+
+int main(){
+    struct t_ponto P, Q;
+    int val;
+    double d;
+
+    printf("Entre com o x do ponto P: ");
+    scanf("%d", &val);
+    P.x = val;
+
+    printf("Entre com o y do ponto P: ");
+    scanf("%d", &val);
+    P.y = val;
+
+    printf("Entre com o x do ponto Q: ");
+    scanf("%d", &val);
+    Q.x = val;
+
+    printf("Entre com o y do ponto Q: ");
+    scanf("%d", &val);
+    Q.y = val;
+
+    d = distancia(P, Q);
+
+    printf("Valor da distância entre P e Q igual a %.2lf\n",d);
+
+
+
+    return 0;
+}
